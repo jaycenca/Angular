642a305c2ea89659917a8e9f496b99f3c183c95e
jaycenca <jaycenca@gmail.com>
2017-09-07 15:08:54 -0400

    Single Page Application: Angular routing part 1 and part 2

---
+++ conFusion/src/app/shared/promotion.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var Promotion = (function () {
+    function Promotion() {
+    }
+    return Promotion;
+}());
+exports.Promotion = Promotion;
