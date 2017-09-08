577920225fe9ddac63f55f234d374e871119fc92
jaycenca <jaycenca@gmail.com>
2017-09-08 17:08:59 -0400

    Assignment 2 officially finished

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -18,6 +18,7 @@ import { DishService } from './services/dish.service';
 import { PromotionService } from './services/promotion.service';
 
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import {LeaderService} from "./services/leader.service";
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +37,7 @@ import { AppRoutingModule } from './app-routing/app-routing.module';
     FlexLayoutModule,
     AppRoutingModule
   ],
-  providers: [ DishService, PromotionService],
+  providers: [ DishService, PromotionService, LeaderService ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- conFusion/src/app/home/home.component.ts
+++ conFusion/src/app/home/home.component.ts
@@ -5,6 +5,10 @@ import { DishService } from '../services/dish.service';
 
 import { Promotion } from '../shared/promotion';
 import { PromotionService } from '../services/promotion.service';
+
+import { Leader } from '../shared/leader';
+import { LeaderService } from '../services/leader.service';
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -14,11 +18,13 @@ export class HomeComponent implements OnInit {
 
   dish: Dish;
   promotion: Promotion;
-  constructor(private dishservice: DishService, private promotionservice: PromotionService) { }
+  leader: Leader;
+  constructor(private dishservice: DishService, private promotionservice: PromotionService, private leaderservice: LeaderService) { }
 
   ngOnInit() {
     this.dish = this.dishservice.getFeaturedDish();
     this.promotion = this.promotionservice.getFeaturedPromotion();
+    this.leader = this.leaderservice.getFeaturedLeader();
   }
 
 }
