3bfd92a1a9c1cadbf6e5699ac1f449730cb67307
jaycenca <jaycenca@gmail.com>
2017-11-08 11:45:18 -0500

    Refactoring the code by creating a new folder for animation and use import to use the speicific animation from the .ts file

---
+++ conFusion/src/app/animations/app.animation.ts
@@ -0,0 +1,16 @@
+import { trigger, state, style, animate, transition } from '@angular/animations';
+
+export function visibility()
+{
+   return trigger('visibility', [
+     state('shown', style({
+       transform: 'scale(1)',
+       opacity: 1
+     })),
+     state('hidden', style({
+       transform: 'scale(.5)',
+       opacity: 0
+     })),
+     transition('* => *', animate('0.5s ease-in-out'))
+   ]);
+}
--- conFusion/src/app/dishdetail/dishdetail.component.ts
+++ conFusion/src/app/dishdetail/dishdetail.component.ts
@@ -11,25 +11,13 @@ import { trigger, state, animate, transition, style } from '@angular/animations'
 
 import 'rxjs/add/operator/switchMap';
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-
+import { visibility } from '../animations/app.animation';
 
 @Component({
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
-  animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in'))
-    ])
-  ],
+  animations: [ visibility() ],
 })
 export class DishdetailComponent implements OnInit {
 
